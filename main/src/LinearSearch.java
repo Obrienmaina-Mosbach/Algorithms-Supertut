4f3a0ca627501023d650235b9dfcfd990a3ae176
Brian Nyawira <191464653+Obrienmaina-Mosbach@users.noreply.github.com>
2025-05-12 13:42:41 +0200

    linear search

---
+++ main/src/LinearSearch.java
@@ -0,0 +1,17 @@
+public class LinearSearch {
+    public static void main(String [] args) {
+        int[] numbers = {23, 89, 67, 34, 82, 232, 42, 13, 94};
+        int target = 82;
+        int result = linearSearch(numbers, target);
+    }
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i =0; i < arr.length; i++){
+            if (arr[i] == target) {
+                System.out.println("Element found at index:" + i);
+                return i;
+            }
+        }
+        return -1;
+    }
+}
